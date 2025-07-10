fc0adcf433b63506fdb2bff2625d52996e289554
aman-2021wb86487 <2021wb86487@wilp.bits-pilani.ac.in>
2025-07-10 17:14:02 +0000

    New Code

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    SECRET_KEY = "2021WB86628"
+    SECRET_KEY = "2021WB86487"
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'Anshul@123'
+    MYSQL_PASSWORD = 'As@#151198'
     MYSQL_DB = 'chatdb'
     MYSQL_CURSORCLASS = 'DictCursor'
     SESSION_COOKIE_SECURE = True
